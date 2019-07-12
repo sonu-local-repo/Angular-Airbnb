b54d63ba98dc611f44a598e805600d3bd4feaa7b
unknown <sonu.speridian@gmail.com>
2019-07-12 13:46:10 -0400

    rental list component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RentalListComponent } from './rentals/rental-list/rental-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: RentalListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { RentalListComponent } from './rentals/rental-list/rental-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RentalListComponent
   ],
   imports: [
     BrowserModule,
