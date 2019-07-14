6e04f83b0175dd2171b9aed72693c52cb9a3b02f
unknown <sonu.speridian@gmail.com>
2019-07-14 08:20:31 -0400

    Model service is added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
+import { ModelService } from './rentals/model/model.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers:[ModelService]
 })
 export class AppComponent {
   title = 'Airbnb';
---
+++ src/app/rentals/model/model.service.ts
@@ -0,0 +1,117 @@
+export class ModelService {
+  rentals: any[] = [ {
+    id: 1,
+    title: "Manson House",
+    city: "New York",
+    street: "Timesqure",
+    category: "house",
+    rate: 250,
+    image:"https://image.shutterstock.com/image-illustration/green-interior-living-room-260nw-174021608.jpg",
+    bedrooms: 5,
+    shared: false,
+    createdAt: "01/01/2019",
+    discreption:"Beautiful home in the city center"
+
+  },
+  {
+    id: 1,
+    title: "Manson House",
+    city: "New York",
+    street: "Timesqure",
+    category: "house",
+    rate: 250,
+    image:"https://static.dezeen.com/uploads/2014/08/House-K-by-K2YT-_dezeen_784_2.jpg",
+    bedrooms: 5,
+    shared: false,
+    createdAt: "01/01/2019",
+    discreption:"Beautiful home in the city center"
+
+  },
+  {
+    id: 1,
+    title: "Manson House",
+    city: "New York",
+    street: "Timesqure",
+    category: "house",
+    rate: 250,
+    image:"https://image.shutterstock.com/image-illustration/green-interior-living-room-260nw-174021608.jpg",
+    bedrooms: 5,
+    shared: false,
+    createdAt: "01/01/2019",
+    discreption:"Beautiful home in the city center"
+
+  },
+  {
+    id: 1,
+    title: "Manson House",
+    city: "New York",
+    street: "Timesqure",
+    category: "house",
+    rate: 250,
+    image:"https://static.dezeen.com/uploads/2014/08/House-K-by-K2YT-_dezeen_784_2.jpg",
+    bedrooms: 5,
+    shared: false,
+    createdAt: "01/01/2019",
+    discreption:"Beautiful home in the city center"
+
+  },   {
+    id: 1,
+    title: "Manson House",
+    city: "New York",
+    street: "Timesqure",
+    category: "house",
+    rate: 250,
+    image:"https://image.shutterstock.com/image-illustration/green-interior-living-room-260nw-174021608.jpg",
+    bedrooms: 5,
+    shared: false,
+    createdAt: "01/01/2019",
+    discreption:"Beautiful home in the city center"
+
+  },
+  {
+    id: 1,
+    title: "Manson House",
+    city: "New York",
+    street: "Timesqure",
+    category: "house",
+    rate: 250,
+    image:"https://static.dezeen.com/uploads/2014/08/House-K-by-K2YT-_dezeen_784_2.jpg",
+    bedrooms: 5,
+    shared: false,
+    createdAt: "01/01/2019",
+    discreption:"Beautiful home in the city center"
+
+  },
+  {
+    id: 1,
+    title: "Manson House",
+    city: "New York",
+    street: "Timesqure",
+    category: "house",
+    rate: 250,
+    image:"https://image.shutterstock.com/image-illustration/green-interior-living-room-260nw-174021608.jpg",
+    bedrooms: 5,
+    shared: false,
+    createdAt: "01/01/2019",
+    discreption:"Beautiful home in the city center"
+
+  },
+  {
+    id: 1,
+    title: "Manson House",
+    city: "New York",
+    street: "Timesqure",
+    category: "house",
+    rate: 250,
+    image:"https://static.dezeen.com/uploads/2014/08/House-K-by-K2YT-_dezeen_784_2.jpg",
+    bedrooms: 5,
+    shared: false,
+    createdAt: "01/01/2019",
+    discreption:"Beautiful home in the city center"
+
+  }];
+
+  getRentals(): any[] {
+    return this.rentals;
+  }
+}
